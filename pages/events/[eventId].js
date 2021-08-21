fbe946956101e7067758447923d54f3e14c374d2
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-08-21 17:33:39 +0700

    Setting up the main pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+function EventShowPage() {
+  return(
+    <div>
+      <h1>Event Show Page</h1>
+    </div>
+  )
+}
+
+export default EventShowPage
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function EventsIndexPage() {
+  return (
+    <div>
+      <h1>Events Index Page</h1>
+    </div>
+  )
+}
+
+export default EventsIndexPage
